035a8c0250dfe4e8c4eef9c61a59c046390ef07f
SouadAlsayed <souadalsayed1804@gmail.com>
2025-09-20 20:59:27 +0300

    fix: adjust UI styling

--- src/features/cart/CartView.tsx
+++ src/features/cart/CartView.tsx
@@ -3,7 +3,7 @@ import CartList from "@/features/cart/CartList";
 
 function CartView() {
   return (
-    <section className="container my-12 mx-auto">
+    <section className="container my-12 mx-auto px-40">
       <h2 className="text-3xl text-center mb-10">Cart</h2>
       <div className="items-start grid grid-cols-3 gap-7">
         <CartList />
--- src/features/home/Shop.tsx
+++ src/features/home/Shop.tsx
@@ -2,7 +2,7 @@ import CardList from "../cards/CardList";
 
 function Shop() {
   return (
-    <section className="my-12 mx-auto">
+    <section className="my-12 mx-auto px-40">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl text-center mb-10">Shop</h2>
 
